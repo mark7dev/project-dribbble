e023649446acd777498e81ef10738393293f4eb5
mark7dev <mcamacho_ce7@live.com.mx>
2018-08-09 13:46:30 -0500

    part3.3

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -7,16 +7,25 @@ class Stats extends React.Component {
         super();
 
         this.state = {
-          likes: props.l
+          likes: props.l,
+          liked: false
         };
     }
       
     incrementLikes = (e) => {
         e.preventDefault();
 
-        this.setState({
-          likes: parseInt(this.state.likes) + 1
-        });
+        if(this.state.liked === false) {
+            this.setState({
+                likes: parseInt(this.state.likes) + 1,
+                liked: true
+            });
+        } else {
+            this.setState({
+                likes: parseInt(this.state.likes) -1,
+                liked: false
+            });
+        }
     }
 
     render () {
@@ -25,7 +34,14 @@ class Stats extends React.Component {
                 <ul className='shots__list'>
                     <li><i className='fa fa-eye' />{ this.props.v }</li>
                     <li><i className='fa fa-comment' />{ this.props.c }</li>
-                    <li><a href='#' onClick={ this.incrementLikes } ><i className='fa fa-heart' />{ this.state.likes }</a></li>
+                    <li>
+                        <a href='#' 
+                            className={ (this.state.liked === true) ? 'liked' : '' }
+                            onClick={ this.incrementLikes } 
+                        >
+                        <i className='fa fa-heart' />{ this.state.likes }
+                        </a>
+                    </li>
                 </ul>
              </div>
         )
