cfd9bb4c00b2c19ba50953b0aa78f0b1195e8bc5
mark7dev <mcamacho_ce7@live.com.mx>
2018-08-09 11:53:16 -0500

    part3.2

--- src/components/stats/Stats.js
+++ src/components/stats/Stats.js
@@ -3,13 +3,29 @@ import React from 'react';
 import './Stats.css';
 
 class Stats extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+          likes: props.l
+        };
+    }
+      
+    incrementLikes = (e) => {
+        e.preventDefault();
+
+        this.setState({
+          likes: parseInt(this.state.likes) + 1
+        });
+    }
+
     render () {
         return (
             <div className='shots__stats'>
                 <ul className='shots__list'>
                     <li><i className='fa fa-eye' />{ this.props.v }</li>
                     <li><i className='fa fa-comment' />{ this.props.c }</li>
-                    <li><i className='fa fa-heart' />{ this.props.l }</li>
+                    <li><a href='#' onClick={ this.incrementLikes } ><i className='fa fa-heart' />{ this.state.likes }</a></li>
                 </ul>
              </div>
         )
